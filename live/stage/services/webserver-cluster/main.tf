ca19c3dbd62df3e7a87737fbc629bd605f03859e
Joe Sheridan <joseph_sheridan@intuit.com>
2018-03-06 15:28:06 -0800

    ????

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -3,3 +3,17 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+
+module "webserver-cluster" {
+    source = "git::https://github.com/jjsheridan/terraform_practice_modules.git//services/webserver-cluster?ref=v0.0.1"
+    ami-id                  = "ami-a9d276c9"
+    server_text             = "Hello, World"
+    cluster_name            = "${var.cluster_name}"
+    db_remote_state_bucket  = "tfstate-${data.aws_caller_identity.current.account_id}-${var.region}"
+    db_remote_state_key     = "stage/data-stores/mysql/terraform.tfstate"
+    port_num                = "${var.port_num}"
+    instance_type           = "t2.micro"
+    min_size                = 2
+    max_size                = 2
+    enable_autoscaling      = false
+    }
